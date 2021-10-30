cc7672b808e7d5f2e38e0f914637a53761e4cc2d
Julia Maksymiuk <76853053+juliaaz@users.noreply.github.com>
2021-10-30 05:09:25 +0300

    Update FlowerBucket.java

--- src/main/java/flowers/FlowerBucket.java
+++ src/main/java/flowers/FlowerBucket.java
@@ -4,11 +4,17 @@ import lombok.ToString;
 import java.util.ArrayList;
 import java.util.List;
 @ToString
-public class FlowerBucket {
+public class FlowerBucket implements Item{
+    String descriptionStr = "";
     public List<FlowerPack> flowerPackList = new ArrayList<FlowerPack>();
 
+    public FlowerBucket(){
+        this.descriptionStr = "Flower Bucket: ";
+    }
     public void addPack(FlowerPack flowerPack){
         flowerPackList.add(flowerPack);
+        this.descriptionStr += flowerPack.descriptionStr + ", ";
+
     }
     public double getPrice() {
         double price = 0;
@@ -16,5 +22,9 @@ public class FlowerBucket {
             price += flowerPack.getPrice();
         }
         return price;
+    }
+
+    public String getDescription(){
+        return this.descriptionStr.toString();
     }
 }
